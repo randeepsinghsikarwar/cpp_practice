fb787acf8857a1ba5714145098adce7a48c59076
HAPPY Happy <randeeps2000@gmail.com>
2023-07-02 13:23:04 +0530

    i am back

---
+++ sublime/largestIntArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// int arr[] = {4,2,5,1,12,8,20};
+	// int largest = 0;
+	// for(int i = 1;i<sizeof(arr)/sizeof(arr[0]);i++){
+	// 	if(arr[i]>arr[largest]){
+	// 		largest = i;
+	// 	}
+	// }
+	// cout<<largest<<endl;
+
+	int a;
+	cout<<"enter";
+	cin>>a;
+	cout<<"asdasd"<<a;
+}
